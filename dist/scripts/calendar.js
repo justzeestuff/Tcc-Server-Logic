2c92cd81977cd99f0a647a581a604a766278cb59
justzeestuff <justsomezeestuff@gmail.com>
2026-08-27 17:10:10 +0400

    first

---
+++ dist/scripts/calendar.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Calendar {
+    constructor() { }
+    Add(data) {
+    }
+}
+//# sourceMappingURL=calendar.js.map
---
+++ src/scripts/calendar.ts
@@ -0,0 +1,17 @@
+interface CalendarStructure{
+    played: number,
+    won: number,
+    draw: number, 
+    lost: number,
+    for: number,
+    against: number,
+    gd: number,
+}
+
+class Calendar{
+    constructor(){}
+
+    Add(data : CalendarStructure[]){
+            
+    }
+}
